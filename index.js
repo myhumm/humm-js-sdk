e2b9123c08a59f183d593ec9601bf0d8743c6fef
MoeBadr <mohamedbadr22@gmail.com>
2015-11-18 20:06:57 +0000

    change of approach :)

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 'use strict';
 
-var config    = require('./src/config');
-/*    artist    =  require('./src/artist'),
-    playlist  =  require('./src/playlist'),
-    users     =  require('./src/users'),
-    songs     =  require('./src/songs');*/
+var config  = require('./src/config'),
+    api     = require('./src/api');
+
 
 module.exports = global.HUMM = {
-  initialize: function initialize(options) {
-    options = options ? options : {};
-    // set tokens
-    config.set('oauth_token', options.oauth_token);
-    config.set('client_id', options.client_id);
-    config.set('redirect_uri', options.redirect_uri);
-    config.set('baseURL', options.baseURL);
-    config.set('connectURL', options.connectURL);
-  }
-/*  users: function users(){
-    return users;
-  },
-  artist: function artist(){
-    return artist;
-  },
-  playlist: function playlist(){
-    return playlist;
-  },
-  songs: function songs(){
-      return songs;
-  }*/
+    initialize: function initialize(options){
+       // options = options ? options : {};
+        // set tokens
+        config.set('oauth_token', options.oauth_token);
+        config.set('client_id', options.client_id);
+        config.set('redirect_uri', options.redirect_uri);
+        config.set('baseURL', options.baseURL);
+        config.set('connectURL', options.connectURL);
+    },
+    get: function get(path, params){
+        return api.request('GET', path, params);
+    },
+
+    post: function post(path, params){
+        return api.request('POST', path, params);
+    },
+
+    put: function put(path, params){
+        return api.request('PUT', path, params);
+    },
+
+    'delete': function _delete(path){
+        return api.request('DELETE', path);
+    }
+
 };
---
+++ src/api.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  request: function request(method, path, params){
+
+
+
+  }
+};
