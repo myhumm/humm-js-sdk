10f9fdb5e556c50696cd0e4157b148967c334308
MoeBadr <mohamedbadr22@gmail.com>
2015-11-20 17:36:39 +0000

    function bodies in progress

--- index.js
+++ index.js
@@ -26,15 +26,37 @@ module.exports = global.humm = {
     settings: settings,
     external: external,
     authorize: authorize,
-    search: function search() {
 
+    /**
+     * TODO: waiting more docs
+     *
+     * @param keyword
+     * @param options
+     * @param cb
+     */
+    search: function search(keyword, options, cb) {
+        var requestData = {
+            url: baseURL + '/search',
+            type: 'GET',
+            params: {
+                keyword: keyword
+            }
+        };
+        request.start(requestData, options, cb)
     },
 
+    /**
+     * TODO: waiting more docs
+     */
     groupSearch: function groupSearch() {
 
     },
 
+    /**
+     * TODO: waiting more docs
+     */
     radio: function radio() {
+
 
     }
 };
@@ -43,3 +65,32 @@ module.exports = global.humm = {
 
 curl --get --include 'https://humm-api.p.mashape.com/artists/55116991f9c816a0d639ea75?auth=564dc328af59fc5215984f7a' \
 -H 'X-Mashape-Key: V9BZDbxMrNmshvvlWRF6izQMMaCqp1L2sdXjsnjlypurv3PWJe'*/
+
+/**
+
+
+ {
+       "group": "Others",
+       "name": "Search",
+       "method": "humm.others.search()",
+       "endpoint": "/search",
+       "usage": "Search all",
+       "returns": "*multiple"
+   },
+ {
+     "group": "Others",
+     "name": "Radio",
+     "method": "humm.others.radio()",
+     "endpoint": "/radio",
+     "usage": "Get a list of songs for a radio",
+     "returns": "songs"
+ },
+ {
+     "group": "Others",
+     "name": "Grouped-search",
+     "method": "humm.others.groupSearch()",
+     "endpoint": "/grouped-search",
+     "usage": "Search all grouped by type",
+     "returns": "*multiple"
+ },
+ */
